865d54c9ed0c7959f6dc9af7dddcdbf2ad1d06fa
NguyenChi2809 <nguyenchi28091995@gmail.com>
2018-04-02 06:03:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Admin</title>
+	<link rel="stylesheet" href="../CSS/style.css">
+	<link rel="stylesheet" href="../Public/Plugin/font-awesome-4.7.0/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../../puclic/plugins/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome.min.css">
+
+	<script src="../Public/Plugin/jquery-3.2.1.js"></script>
+	<script src="../JS/main.js"></script>
+</head>
+<body>
+	<?php require'header.php';?>
+	<!-- begin main -->
+	<!-- <main>
+		<?php require'menu.php';?> -->
+		<!-- content right -->
+		<section class="content">
+			<div class="content-header">
+				<div class=" title-message col-3">
+					<div class="message-header">
+						<span>26 new message</span>
+						<i class="fa fa-comments" aria-hidden="true"></i>
+					</div>
+					<div class="message-end">
+						<div class="end-left">
+							View detail
+						</div>
+						<div class="end-right">
+							<i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- end content right -->
+	</main>
+	<!-- end main -->
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -1,132 +1,65 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
-	 <style>
-          * {
-            box-sizing:border-box
-          }
-          h2 {
-            text-align: center;
-          }
-          /* Slideshow container */
-          .slideshow-container {
-            max-width: 500px;
-            position: relative;
-            margin: auto;
-          }
-          /* Ẩn các slider */
-          .mySlides {
-              display: none;
-          }
-          /* Định dạng nội dung Caption */
-          .text {
-            color: #f2f2f2;
-            font-size: 15px;
-            padding: 8px 12px;
-            position: absolute;
-            bottom: 8px;
-            width: 100%;
-            text-align: center;
-          }
-
-          /* định dạng các chấm chuyển đổi các slide */
-          .dot {
-            cursor:pointer;
-            height: 13px;
-            width: 13px;
-            margin: 0 2px;
-            background-color: #bbb;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-          }
-          /* khi được hover, active đổi màu nền */
-          .active, .dot:hover {
-            background-color: #717171;
-          }
-
-          /* Thêm hiệu ứng khi chuyển đổi các slide */
-          .fade {
-            -webkit-animation-name: fade;
-            -webkit-animation-duration: 3s;
-            animation-name: fade;
-            animation-duration: 3s;
-          }
-
-          @-webkit-keyframes fade {
-            from {opacity: .4} 
-            to {opacity: 1}
-          }
-
-          @keyframes fade {
-            from {opacity: .4} 
-            to {opacity: 1}
-          }
-        </style>
-</head>
-<body>
-	<div class="slideshow-container">
-        <h2>Freetus.net hướng dẫn tạo slideShow đơn giản</h2>
-        <div class="mySlides fade">
-          <img src="image/banner-demo.jpg" style="width:100%">
-          <div class="text">Nội dung caption của slide đầu tiên!</div>
-        </div>
-
-        <div class="mySlides fade">
-          <img src="image/c052a-ty-huu-xanh-1-90x90.jpg" style="width:100%">
-          <div class="text">Nội dung caption của slide thứ 2!</div>
-        </div>
-
-        <div class="mySlides fade">
-          <img src="image/c212a-1-chuot-sa-kim-1-90x90.jpg" style="width:100%">
-          <div class="text">Nội dung caption của slide thứ 3!</div>
-        </div>
-      </div>
-      <br>
-
-      <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(0)"></span> 
-        <span class="dot" onclick="currentSlide(1)"></span> 
-        <span class="dot" onclick="currentSlide(2)"></span> 
-      </div>  
-    </body>
-    <script>
-      //khai báo biến slideIndex đại diện cho slide hiện tại
-      var slideIndex;
-      // KHai bào hàm hiển thị slide
-      function showSlides() {
-          var i;
-          var slides = document.getElementsByClassName("mySlides");
-          var dots = document.getElementsByClassName("dot");
-          for (i = 0; i < slides.length; i++) {
-             slides[i].style.display = "none";  
-          }
-          for (i = 0; i < dots.length; i++) {
-              dots[i].className = dots[i].className.replace(" active", "");
-          }
-
-          slides[slideIndex].style.display = "block";  
-          dots[slideIndex].className += " active";
-          //chuyển đến slide tiếp theo
-          slideIndex++;
-          //nếu đang ở slide cuối cùng thì chuyển về slide đầu
-          if (slideIndex > slides.length - 1) {
-            slideIndex = 0
-          }    
-          //tự động chuyển đổi slide sau 5s
-          setTimeout(showSlides, 5000);
-      }
-      //mặc định hiển thị slide đầu tiên 
-      showSlides(slideIndex = 0);
-
-
-      function currentSlide(n) {
-        showSlides(slideIndex = n);
-      }
-    </script>
-</body>
-</html>
+<!-- begin menu --> 
+<aside class="navbar">
+	<nav>
+			<ul class="menu">
+				<li>
+					<a href="#">
+						<i class="icon icon-left fa fa-home" aria-hidden="true"></i>Trang chủ
+					</a>
+				</li>
+				<li>
+					<a href="../Catalog/catalog.php">
+						<i class="icon icon-left fa fa-file-text-o" aria-hidden="true"></i>
+						Quản lý danh mục
+					</a>
+				</li>
+				<li>
+					<a href="../slider/images.php">
+						<i class="icon icon-left fa fa-picture-o" aria-hidden="true"></i>
+						Quản lý hình ảnh
+					</a>
+				</li>
+				<li>
+					<a href="../product/product.php">
+						<i class="icon icon-left fa fa-bars" aria-hidden="true"></i>
+						Quản lý sản phẩm
+					</a>
+				</li>
+				<li>
+					<a href="../bill/bill.php">
+						<i class="icon icon-left fa fa-shopping-basket" aria-hidden="true"></i>
+						Quản lý đơn hàng
+						<!-- <i class="fa icon-right down fa-angle-down" id="icon-down" aria-hidden="true"></i>
+						<i class="icon icon-right down-right fa fa-angle-right" aria-hidden="true"></i> -->
+					</a>
+					<!-- <ul>
+						<li><a href="#">Lọ hoa</a></li>
+						<li><a href="#">ấm chén</a></li>
+						<li><a href="#">bát đĩa</a></li>
+						<li><a href="#">tượng</a></li>
+					</ul> -->
+				</li>
+				<li>
+					<a href="../blog/new.php">
+						<i class="icon icon-left fa fa-newspaper-o" aria-hidden="true"></i>
+						Quản lý mail Khách hàng
+						<!-- <i class="fa icon-right down fa-angle-down" id="icon-down" aria-hidden="true"></i>
+						<i class="icon icon-right down-right fa fa-angle-right" aria-hidden="true"></i> -->
+					</a>
+				</li>
+				<li>
+					<!-- <a href="#">
+						<i class="icon icon-left fa fa-bar-chart" aria-hidden="true"></i>
+						Thống kê
+					</a> -->
+				</li>
+				<li>
+					<!-- <a href="#">
+						<i class="icon icon-left fa fa-upload" aria-hidden="true"></i>
+						Import File
+					</a>
+				</li> -->
+			</ul>
+		</nav>
+</aside>
+<!-- end menu
